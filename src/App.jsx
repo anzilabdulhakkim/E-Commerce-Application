f7725fe3c7ecb7dd536f7ea7e64e6317d1f56d4d
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-03-29 23:32:31 +0530

    added app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import Allroutes from "./Routes/Allroutes";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Allroutes />
+    </>
+  );
+}
+
+export default App;
